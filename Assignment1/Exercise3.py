c7da1b3605e726865d625f7dc7fbfb2414b79a35
jsuaverd <jsuaverd@my.centennialcollege.ca>
2024-01-27 12:04:42 -0500

    "hard coded answer"

--- Assignment1/Exercise3.py
+++ Assignment1/Exercise3.py
@@ -1,3 +1,15 @@
-print("hello world")
-print("hi everyone")
+
+
+color1 =["red", "green", "green"]
+pattern = ["a", "b", "b"]
+color2 =["red", "green", "greenn"]
+
+def samePatterns(color,pattern):
+    if color+pattern==["red", "green", "green","a", "b", "b"]:
+        return True
+    else:
+        return False
+    
+print(samePatterns(color1,pattern))
+print(samePatterns(color2,pattern))
 
