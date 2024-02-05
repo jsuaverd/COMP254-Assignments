27e6f018dbb770d9678dc783ce9aa8706b083eae
DMBrar <dmvbrar@gmail.com>
2024-02-05 16:32:39 -0500

    update exercise 2

--- Assignment2/Exercise2.py
+++ Assignment2/Exercise2.py
@@ -1,8 +1,11 @@
 #The difference between a shallow copy and a deep copy is that in a shallow copy, we create a new collection and populate it with REFERENCES to child objects from the original. This means that any changes made to the copy also reflects on the original, and vice versa. Whereas in a deep copy, we create a new collection and recursively creates copies of the objects from the original. This means that the original and the copy are independent of each other and any changes made on either do not reflect on the other.
 
-#Shallow copy
+
 # importing the module
 from numpy import array
+
+print('\nSHALLOW COPY --------------------------------------------------------------------------')
+#Shallow copy-----------------------------------------------------------------------------
 # creating the first array
 arr1 = array(['ant', 'bat', 'cat', 'dog', 'emu'])
 # displaying the identity of arr1
@@ -30,3 +33,30 @@ for item in arr2:
     print(item)
 
 
+print('\nDEEP COPY --------------------------------------------------------------------------')
+#Deep copy-----------------------------------------------------------------------------
+# creating the first array
+arr3 = array([1001,2002,3003,4004,5005,6006])
+# displaying the identity of arr3
+print('\nArray 3 (ID:' +  str(id(arr3)) + '):')
+for item in arr3:
+    print(item)
+print('--------------------------------------------------------------------------')
+# deep copy arr1 in arr4 using copy()
+arr4 = arr3.copy()
+# displaying the identity of arr4
+print('\nCopy of Array 1 (ID:' +  str(id(arr4)) + '):')
+for item in arr4:
+    print(item)
+print('--------------------------------------------------------------------------')
+# making a change in arr3
+arr3[2] = 8008
+# displaying the arrays
+print('\nAfter making a change in Array 3:')
+print('\nArray 3:')
+for item in arr3:
+    print(item)
+print('--------------------------------------------------------------------------')
+print('\nArray 4:')
+for item in arr4:
+    print(item)
