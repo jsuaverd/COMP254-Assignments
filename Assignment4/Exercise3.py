97cb2f84e29a50193af65e2dd104ad75d2e9b355
jsuaverd <jsuaverd@my.centennialcollege.ca>
2024-03-15 10:52:54 -0400

    BFS updated

--- Assignment4/Exercise3.py
+++ Assignment4/Exercise3.py
@@ -1,5 +1,3 @@
-# Traverse Binary Tree in Python using DFS 
-
 class Node:
     def __init__(self, key):
         self.left = None
@@ -25,36 +23,32 @@ class Node:
     # Traverse postorder
     def traversePostOrder(self):
         if self.left:
-            
             self.left.traversePostOrder()
-        
         if self.right:
             self.right.traversePostOrder()
-
         print(self.val, end=' ')
 
-    def traverseBFS():
-        pass
+    # Traverse BFS
+    def traverseBFS(self):
+        queue = [self]  # Start with the root node
+        while queue:
+            current_node = queue.pop(0)  # Dequeue the first node
+            print(current_node.val, end=' ')
+            if current_node.left:
+                queue.append(current_node.left)  # Enqueue left child
+            if current_node.right:
+                queue.append(current_node.right)  # Enqueue right child
 
-            
-        
-
-    
-
-
+# Example usage
 root = Node(6)
-
 root.left = Node(2)
 root.right = Node(8)
-
 root.left.left = Node(1)
 root.left.right = Node(4)
 root.left.right.left = Node(3)
 root.left.right.right = Node(5)
-
 root.right.left = Node(7)
 root.right.right = Node(9)
-
 
 print("Pre order Traversal: ", end="")
 root.traversePreOrder()
@@ -62,6 +56,5 @@ print("\nIn order Traversal: ", end="")
 root.traverseInOrder()
 print("\nPost order Traversal: ", end="")
 root.traversePostOrder()
-print("\n BFS Traversal: ", end="")
-#root.traverseBFS()
-
+print("\nBFS Traversal: ", end="")
+root.traverseBFS()
